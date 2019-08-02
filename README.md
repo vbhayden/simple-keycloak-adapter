a1e0087f9c8dcfb2e8bd1d50e734cc5f91792882
adl-trey <trey.hayden.ctr@adlnet.gov>
2019-08-02 14:48:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+## Simplified Keycloak Adapter
+This is a Node module to streamline Keycloak integration Node+Express based services.  It's intended to abstract some of the boilerplate necessary to protect Express endpoints when using a Keycloak instance for SSO, but does still require some configuration to specify which instance to target.
+
+### Installation
+While this isn't on NPM directly, you can install it through this repo:
+```
+npm install https://github.com/vbhayden/simple-keycloak-adapter --save
+```
+
+### Setup
+Setup will depend on which Keycloak client you are using to protect your endpoints.  Assuming you're using a client called `my-client` on some realm `my-realm` running locally on port `8080`, your first step will be something like:
+
+```
+const keycloakHelper = require("simple-keycloak-adapter");
+
+app.use(keycloakHelper.init({
+    realm: "my-realm",
+    authServerUrl: "http://localhost:8080/auth",
+    client: "my-client"
+}));
+```
+where `app` is the typical Express application instance.  Once this is in place, subsequent routes can be protected individually:
+```
+app.get("/protected", keycloakHelper.protect(), (req, res, next) => res.send("protected endpoint!"))
+```
+or through a catchall that will protect all routes declared after it:
+```
+app.use(keycloakHelper.protect());
+```
+
+### Example
+For a full example using the above blocks:
+```
+const keycloakHelper = require("simple-keycloak-adapter");
+const app = require("express")()
+
+// Initialize our middleware
+app.use(keycloakHelper.init({
+    realm: "my-realm",
+    authServerUrl: "http://localhost:8080/auth",
+    client: "my-client"
+}));
+
+// Set up two routes, with the root being unprotected
+app.get("/", (req, res, next) => res.send("unprotected endpoint!"))
+app.get("/protected", keycloakHelper.protect(), (req, res, next) => res.send("protected endpoint!"))
+
+// Start our server
+app.listen(3000, () => console.log("listening on 3000!"))
+```
+### Optional Syntax
+For clarity and as the underlying `keycloak-connect` library depends on hyphenated property names (`"auth-server-url"`), there are aliases provided.  When you pass one of these aliased versions, they will be assigned to their syntactically-correct equivalent before being passed to Keycloak.
+
+| Adapter Property | Keycloak Property | 
+|:----------|:------|
+| `client`| `resource`|
+| `authServerUrl`| `auth-server-url`|
+| `bearerOnly`| `bearer-only`|
+| `confidentialPort`| `confidential-port`|
+| `publicClient`| `public-client`|
+| `sslRequired` | `ssl-required` |
+
+### Logout and Redirect endpoints
+By default, the middleware will react to a specified path and treat that as a logout request.  We default this to `/logout`, but this can be set manually during the `.init()` call.  For example, something whose base was `/registry` might use the following endpoints:
+```
+app.use(keycloakHelper.init(config.keycloak, "/registry/logout", "/registry"))
+```
+where the first argument is the logout route and the second is where unauthorized users will be directed.
+
+### Access Denial Handler
+You can supply another optional argument to that `.init()` call to specify how a denied request should proceed.  By default, this just redirects the user to the root path, but you can supply your own:
+```
+let logoutHandler = (req, res, next) => res.send("unauthorized!");
+app.use(keycloakHelper.init({ ... }, "/logout", "/", logoutHandler))
+```
+
---
+++ index.js
@@ -0,0 +1,114 @@
+const session = require("express-session");
+const Keycloak = require("keycloak-connect");
+
+const memory = new session.MemoryStore();
+var keycloak = undefined;
+
+const defaults = {
+    "ssl-required": "none",
+    "public-client": true,
+    "confidential-port": 0
+}
+
+/**
+ * TLA Keycloak Helper.
+ * 
+ * Set of middleware / request handlers for protecting endpoints with the TLA Keycloak instance.
+ */
+module.exports = {
+
+    /**
+     * Set of credentials needed to access a confidential resource.
+     * @typedef {Object} KeycloakCredentials
+     * @property {string} secret - Secret key used for accessing this client.
+     */
+    
+    /**
+     * Object defining what Keycloak instance our adapter should target and how.
+     * 
+     * @typedef {Object} KeycloakConfig 
+     * @property {KeycloakCredentials} credentials - Credentials necessary (secret).
+     * @property {boolean} bearerOnly - (Alias for "bearer-only") Is this a "bearer-only" resource? Default: undefined
+     * @property {string} authServerUrl - (Alias for "auth-server-url") Location of the auth server. 
+     * @property {string} sslRequired - (Alias for "ssl-required") Does this requires SSL?  Default: "none"
+     * @property {string} resource - Name of the Keycloak client.
+     * @property {string} client - (Alias for "resource") Name of the Keycloak client.
+     * @property {boolean} publicClient - (Alias for "public-client") Is this a public client? Default: true
+     * @property {number} confidentialPort - (Alias for "confidential-port") Not sure what this does, set to 0. Default: 0
+     */
+
+    /**
+     * Initialize the Keycloak middleware for site-wide coverage.
+     * 
+     * @param {KeycloakConfig} config 
+     * @param {string} logout 
+     * @param {string} redirect 
+     * @returns {RequestHandler[]} handlers
+     */
+    init: function(config, logout="/logout", redirect="", accessDenied=undefined) {
+
+        // Fix our hyphen values
+        if (config["auth-server-url"] == undefined)
+            config["auth-server-url"] = config.authServerUrl;
+
+        if (config["ssl-required"] == undefined)
+            config["ssl-required"] = config.sslRequired;
+            
+        if (config["public-client"] == undefined)
+            config["public-client"] = config.publicClient;
+
+        if (config["confidential-port"] == undefined)
+            config["confidential-port"] = config.confidentialPort;
+
+        if (config["bearer-only"] == undefined && config.bearerOnly != undefined)
+            config["bearer-only"] = config.bearerOnly;
+        
+        if (config["resource"] == undefined && config.client != undefined)
+            config["resource"] = config.client;
+
+        // Instance of the Keycloak object to use.
+        keycloak = new Keycloak({store: memory}, {...defaults, ...config});
+
+        // How to handle when our user isn't allowed onto this resource
+        keycloak.accessDenied = accessDenied != undefined ? accessDenied : (req, res, next) => {
+            res.redirect(redirect)
+        }
+
+        // Return a request handler using both of these
+        return [
+            session({
+                secret: "secret",
+                resave: false,
+                saveUninitialized: true,
+                store: memory
+            }),
+            keycloak.middleware({
+                logout: logout
+            })
+        ]
+    },
+
+    /**
+     * Requires a Keycloak login for anything accessing this resource.  Upon login, the
+     * response object will contain the Keycloak user's information in res.locals.user.
+     * 
+     * That object has the following properties: id, name, admin, and author.
+     */
+    protect: function() {
+        return (req, res, next) => {
+            function confirmRoles(token, request) {
+
+                res.locals.user = {
+                    id: token.content.sub,
+                    name: token.content.preferred_username,
+                    admin: token.hasRole("realm:admin"),
+                    author: token.hasRole("realm:author"),
+                }
+
+                return true;
+            }
+
+            keycloak.protect(confirmRoles)(req, res, next);
+        }
+    }
+}
